c4b6856bbb8829ca6055d713fbc2984622cc38a1
XayahP <ycxiang14sep@gmail.com>
2022-01-29 02:22:24 +0800

    update eigen

--- cmake/install_eigen.cmake
+++ cmake/install_eigen.cmake
@@ -4,6 +4,6 @@ ExternalProject_Add(
         GIT_TAG 46126273552afe13692929523d34006f54c19719
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ""
-        INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory ${EXTERNAL_PREFIX}/eigen3/src/eigen3/Eigen ${EXTERNAL_INSTALL_DIR}/include/Eigen
+        INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory ${EXTERNAL_PREFIX}/eigen3/src/eigen3 ${EXTERNAL_INSTALL_DIR}/include/eigen3
         PREFIX ${EXTERNAL_PREFIX}/eigen3
 )
